10d489499fb6c29d6ca9ad59a6bd413312989066
anttiey <hanuuny@gmail.com>
2024-07-15 15:21:36 +0900

    feat: 기본 라우팅 및 공통 스타일 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,32 @@
+const color = {
+  primary: {
+    light: '#F1FBFA',
+    main: '#00E0C8',
+  },
+  secondary: '#FFC453',
+};
+
+const fontSize = {
+  sm: '12px',
+  base: '14px',
+  md: '16px',
+  lg: '24px',
+};
+
+const fontWeight = {
+  light: '400',
+  medium: '500',
+  semibold: '600',
+  bold: '700',
+};
+
+const boxHeight = '64px';
+
+const theme = {
+  color,
+  fontSize,
+  fontWeight,
+  boxHeight,
+};
+
+export default theme;
