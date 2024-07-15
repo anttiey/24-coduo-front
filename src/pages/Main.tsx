24a97a3f5b2e354f58a28bdb64d66986f4313909
anttiey <hanuuny@gmail.com>
2024-07-15 16:12:48 +0900

    refactor: 메인 페이지 폴더 구조 변경

--- src/pages/Main.tsx
+++
@@ -1,5 +0,0 @@
-const Main = () => {
-  return <div>코딩해듀오</div>;
-};
-
-export default Main;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import * as S from './Main.style';
+
+const Main = () => {
+  return (
+    <S.Layout>
+      <h1>코딩해듀오</h1>
+      <p>협업의 힘을 경험하세요. 코딩해듀오가 도와드립니다!</p>
+      <button>방 만들기</button>
+    </S.Layout>
+  );
+};
+
+export default Main;
