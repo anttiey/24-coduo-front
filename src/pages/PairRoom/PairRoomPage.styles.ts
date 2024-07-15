fdcc2ade3a6dad8731173e61fa1adae459512ad3
greetings1012 <ahnqwerty@gmail.com>
2024-07-15 20:49:15 +0900

    ✨ 페어룸 페이지 구현

---
+++ src/pages/PairRoom/PairRoomPage.styles.ts
@@ -0,0 +1,122 @@
+import styled, { css, keyframes } from 'styled-components';
+
+const blink = keyframes`
+  0%, 100% {
+    background-color: #FFC453; /* Original color */
+  }
+  50% {
+    background-color: #ff6951;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const TimerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin-bottom: 3rem;
+`;
+
+export const Timer = styled.div<{ progress: number; isTimeUp?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28rem;
+  height: 28rem;
+  border-radius: 50%;
+  background: conic-gradient(#00e0c8 ${({ progress }) => progress}%, #ddd ${({ progress }) => progress}%);
+  position: relative;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 24rem;
+    height: 24rem;
+    background: #fff;
+    border-radius: 50%;
+    z-index: -1;
+  }
+`;
+
+export const TimerText = styled.p<{ isTimeUp?: boolean }>`
+  font-size: 4rem;
+  font-weight: bold;
+  color: #000;
+  ${({ isTimeUp }) =>
+    isTimeUp &&
+    css`
+      color: #ff6951;
+    `}
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.button<{ isTimeUp?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  margin: 0.5rem;
+  border: none;
+  border-radius: 50%;
+  background-color: #ffc453;
+  cursor: pointer;
+  font-size: 1.5rem;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #db9e3c;
+  }
+
+  &:active {
+    background-color: #b87c2a;
+  }
+
+  &:disabled {
+    background-color: #c0c0c0;
+    cursor: not-allowed;
+  }
+
+  ${({ isTimeUp }) =>
+    isTimeUp &&
+    css`
+      animation: ${blink} 1s infinite;
+    `}
+`;
+
+export const RoleTextContainer = styled.div`
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+`;
+
+export const RoleText = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  color: #333333;
+  margin: 5px 0;
+  text-align: center;
+  line-height: 5rem;
+  span {
+    color: #006c81;
+  }
+`;
---
+++ src/pages/PairRoom/PairRoomPage.tsx
@@ -0,0 +1,104 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+import { VscDebugStart, VscDebugPause } from 'react-icons/vsc';
+import { CgArrowsExchange } from 'react-icons/cg';
+import * as S from './PairRoomPage.styles';
+
+const TIMER = 10000;
+const USER = [
+  { name: '파슬리', role: 'navigator' },
+  { name: '해시', role: 'driver' },
+];
+
+const PairRoomPage = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [driver, setDriver] = useState(USER.find((user) => user.role === 'driver'));
+  const [navigator, setNavigator] = useState(USER.find((user) => user.role === 'navigator'));
+  const [time, setTime] = useState(TIMER);
+  const startTime = useRef<number | null>(null);
+  const pauseTime = useRef<number | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleChangeRole = () => {
+    setDriver(navigator);
+    setNavigator(driver);
+    startTime.current = null;
+    pauseTime.current = null;
+    handleStart();
+  };
+
+  const handleStart = useCallback(() => {
+    if (startTime.current === null) {
+      startTime.current = Date.now();
+    } else if (pauseTime.current !== null) {
+      startTime.current += Date.now() - pauseTime.current;
+    }
+    setIsRunning(true);
+  }, [startTime, pauseTime]);
+
+  const handlePause = useCallback(() => {
+    if (startTime.current !== null) {
+      pauseTime.current = Date.now();
+    }
+    setIsRunning(false);
+  }, [startTime, pauseTime]);
+
+  const getTime = useCallback(() => {
+    if (startTime.current === null) return TIMER;
+    const elapsed = isRunning ? Date.now() - startTime.current : pauseTime.current! - startTime.current;
+    return Math.max(TIMER - elapsed, 0);
+  }, [isRunning]);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTime(getTime());
+      }, 20);
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [isRunning, getTime, time]);
+
+  const progress = (time / TIMER) * 100;
+  const isTimeUp = time === 0;
+
+  return (
+    <S.Container>
+      <S.RoleTextContainer>
+        <S.RoleText style={{ textAlign: 'left' }}>
+          드라이버 <br />
+          <span>{driver?.name}</span>
+        </S.RoleText>
+        <S.RoleText style={{ textAlign: 'right' }}>
+          내비게이터 <br />
+          <span>{navigator?.name}</span>
+        </S.RoleText>
+      </S.RoleTextContainer>
+      <S.TimerContainer>
+        <S.Timer progress={progress} isTimeUp={isTimeUp}>
+          <S.TimerText isTimeUp={isTimeUp}>
+            {Math.floor(time / 60000)}:{('0' + (Math.floor(time / 1000) % 60)).slice(-2)}
+            {time < 60000 && <>.{('00' + (time % 1000)).slice(-3)}</>}
+          </S.TimerText>
+        </S.Timer>
+      </S.TimerContainer>
+      <S.ButtonWrapper>
+        <S.Button onClick={handleStart} disabled={isRunning || isTimeUp}>
+          <VscDebugStart />
+        </S.Button>
+        <S.Button onClick={handlePause} disabled={!isRunning || isTimeUp}>
+          <VscDebugPause />
+        </S.Button>
+        <S.Button onClick={handleChangeRole} isTimeUp={isTimeUp}>
+          <CgArrowsExchange />
+        </S.Button>
+      </S.ButtonWrapper>
+    </S.Container>
+  );
+};
+
+export default PairRoomPage;
